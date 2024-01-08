ac33d2427f7a7505f58cd3d4b338c19caaa4bdd3
John Viscel M. Sangkal <jvmsangkal@gmail.com>
2024-01-08 10:55:53 +0800

    first commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "cmake",
+        "cpp",
+        "css",
+        "fish",
+        "gitignore",
+        "go",
+        "graphql",
+        "http",
+        "java",
+        "php",
+        "rust",
+        "scss",
+        "sql",
+        "svelte",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "MDX",
+        },
+      })
+
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
